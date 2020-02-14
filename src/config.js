1b62138f6b15d35c816b9dd55e69c7f3592909ee
nesha95 <pijanic.nenad@gmail.com>
2020-02-14 10:37:17 +0100

    Building our React app without Veljko Simic

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
+
     s3: {
       REGION: "us-east-2",
       BUCKET: "notes-app-objectuploads"
@@ -12,5 +14,7 @@ export default {
       USER_POOL_ID: "us-east-2_J7PZliyRp",
       APP_CLIENT_ID: "324uood0nqe73s7p1u3s1ub5ob",
       IDENTITY_POOL_ID: "us-east-2:4c1c3719-3c66-416d-9dae-8cda75b43f54"
-    }
+    },
+
+    STRIPE_KEY: "pk_test_ihzI6WykZJJNmtDvIpHwavjQ00wcmZ3J7C"
   };
